afd6e186ab673e6fbe518201072a4e7b467bb651
Kuzkoviy <72725653+Kuzkoviy@users.noreply.github.com>
2024-08-05 21:04:28 +0300

    erorr mesage added

---
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+const ErrorMessage = () => {
+  return (
+    <div>
+        <h1>Some error happend</h1>
+        <p>Try to reload your page</p>
+    </div>
+  )
+}
+
+export default ErrorMessage
